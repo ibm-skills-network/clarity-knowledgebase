c74964cb63c0bed23f4083b34b787ce7eaeba1d2
Michael Roudnitski <mroudnitski@ibm.com>
2022-04-13 10:10:52 -0400

    update editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/ibm-skills-network/author-workbench-knowledgebase/tree/master',
         },
         blog: false,
         theme: {
