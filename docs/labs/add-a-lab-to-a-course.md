1fef1eeed84c833ba6287cf8b1e106cf60640de7
Michael Roudnitski <mroudnitski@ibm.com>
2022-04-21 14:18:40 -0400

    chore: remove boilterplate

--- docs/labs/add-a-lab-to-a-course.md
+++ docs/labs/add-a-lab-to-a-course.md
@@ -4,4 +4,4 @@ sidebar_position: 2
 
 # Add a Lab to your Course
 
-WIP, same format as previous page. We will show authors how to add a lab to a course in Studio using LTI.
+Once you have created a lab, you will **add it to your course via Studio**. When you are adding a lab to your course, you **embed your lab into a unit of your course**. This way, learners will naturally progress through the course, and complete your labs as they go.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,35 +60,8 @@ const config = {
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} Skills Network. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
